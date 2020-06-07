f86bc9a5e5f45d16aba4964fa0c67cb0378044a1
neo.xia <neo.xia@gz.questwork.com>
2020-06-07 23:44:05 +0800

    first config
    border.css解决移动端1物理像素边框问题；
    reset.css重设所有默认样式，适配各种移动端；
    fastclick解决移动点击300ms延迟

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="home">home</div>
+    <router-link to="/list" class="home">跳转列表页</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'home'
+}
+</script>
+
+<style>
+.home {
+  font-size: 50px;
+}
+</style>
